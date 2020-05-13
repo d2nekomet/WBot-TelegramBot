99899dfc67c8d959d46ead07fc652a61d7bc1b49
OSPeople <62840755+d2nekomet@users.noreply.github.com>
2020-05-13 17:09:31 +0300

    v1.5
    Changelog:
    Сохранение токена и id в отдельном .ini файле.
    Сохранение прочих файлов (idlist.txt, friends.txt и др.) в отдельной папке
    Фикс бага с файлами логов и их сохранением по времени.
    При вводе в /dbupload неверных данных бот просто попросит перепроверить то, что отправил админ.
    Обновил install.py
    English(Google translate):
    Saving and token id in a separate .ini file.
    Keeping other files (idlist.txt, friends.txt et al.) In a separate folder
    Fixed bug with log files and saving them time.
    When you type in / dbupload misrepresentation boat just ask to double-check that sent the admin.
    updated install.py

--- bot.py
+++ bot.py
@@ -1,68 +1,87 @@
-#Бот канала t.me/www_project 
-#Написан t.me/os_people
-#Emoji for message      	➡	⬅🔃  💻📷 📸 📹 🎥 📽💡💣🧱📁 📂 🗂🔍 🔎 🔏 🔐 🔒 🔓❤️ 🧡 💛 💚 💙 💜 🖤☢️ ☣️ 📴 📳🆔  ✉️ 📩 📨 📧 💌  📱 📲 💻 ⌨️ 🖥 🖨
-##########################
+#Channel: t.me/www_project 
+#Coder: t.me/os_people
+#Emoji ➡⬅🔃💻📷📸📹🎥📽💡💣🧱📁📂🗂🔍🔎🔏🔐🔒🔓❤️🧡💛💚💙💜🖤☢️☣️📴📳🆔✉️📩📨📧💌📱📲💻⌨️🖥🖨
+
 import time
 import os
 import telebot
-import logging               # Модули для работы бота 
-from telebot import types
-##########################
+from telebot import *
+import logging
+import threading
+import configparser
 logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'w+')
-logs.close()                                         #создание файла логов
-logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'a+')
+logs.close()
+logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'w+')
+idslist = open('Other/idlist.txt', 'w+')
 
-idslist = open('idlist.txt', 'a+')
+conf = configparser.RawConfigParser()
+conf.read("Config/config.ini")
+print('Ваш токен: '+conf.get("Bot", "token"))
+print('Ваш id: '+conf.get("Bot", "adminid"))
+TOKEN = conf.get("Bot", "token")
+adminid = int(conf.get("Bot", "adminid"))
+idslist = open('Other/idlist.txt', 'w+')
 
-tokenfile = open('Config/token.txt', 'r')
-TOKEN = tokenfile.read()
+bot = telebot.TeleBot(TOKEN)
 
-adminidf = open('Config/adminid.txt', 'r')
-adminidr = adminidf.read()
-print('Ваш TelegramID: ' + adminidr + '\nВаш токен бота: ' + TOKEN)
+def friend_base(message):
+	global friendbase
+	friendbase = open('Other/friends.txt','a+')
+	friendbase.write(message.text + '\n')
+	friendbase.close()                           #это обновление списка баз данных
+	friendbase = open('Other/friends.txt','r')
+	bot.send_message(message.chat.id, 'Список друзей обновлен!\nВот он: \n' + friendbase.read())
+	friendbase.close()
 
-idslist = open('idlist.txt', 'a+')
-bot = telebot.TeleBot(TOKEN) # TOKEN 
-adminid = int(adminidr)# TelegramID админа
-####################
 def url_links(message):
-	links = open('links.txt','a+')
+	links = open('Other/links.txt','w+')
 	links.write(message.text + '\n')
-	links.close()                           # это у нас функция обновления списка хоста.
-	links = open('links.txt','r')
+	links.close()
+	links = open('Other/links.txt','r')
 	bot.send_message(message.chat.id, 'Список софта обновлен!\nВот он: \n' + links.read())
 	links.close()
 
 def url_base(message):
-	base = open('base.txt','a+')
+	base = open('Other/base.txt','w+')
 	base.write(message.text + '\n')
-	base.close()                           #это обновление списка баз данных
-	base = open('base.txt','r')
+	base.close()
+	base = open('Other/base.txt','r')
 	bot.send_message(message.chat.id, 'Список баз данных обновлен!\nВот он: \n' + base.read())
 	base.close()
-def friend_base(message):
-	global friendbase
-	friendbase = open('friends.txt','a+')
-	friendbase.write(message.text + '\n')
-	friendbase.close()                           #это обновление списка баз данных
-	friendbase = open('friends.txt','r')
-	bot.send_message(message.chat.id, 'Список друзей обновлен!\nВот он: \n' + friendbase.read())
-	friendbase.close()
 
 def dbupl(message):
 	global logs, dbl
-	logs.close()
-	bot.send_message(message.chat.id, '📁Отправляю файл!📁\n📁Имя файла: Logs/' + str(message.text) + ' 📁')
-	dbl = open('Logs/' + str(message.text), 'rb')
-	bot.send_document(adminid, dbl)       # это дампит логи
-	dbl.close()
-	logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'a+')
+	if os.path.exists('Logs/' + str(message.text)) == True:
+		logs.close()
+		bot.send_message(message.chat.id, '📁Отправляю файл!📁\n📁Имя файла: Logs/' + str(message.text) + ' 📁')
+		dbl = open('Logs/' + str(message.text), 'r')
+		bot.send_document(adminid, dbl)
+		dbl.close()
+		logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'w+')
+	else:
+		bot.send_message(adminid, 'Нет такого файла!\nПожалуйста, введите команду /dbupload с корректными данными!')
 
-##################
+def restartlog():
+	logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'w+')
+	logs.close()
+	logs = open('Logs/log' + time.strftime('%B%d%Y', time.localtime()) + '.txt', 'w+')
+
+def logres():
+	while True:
+		if time.strftime("%H:%M:%S") == '00:00:01':
+			bot.send_message(adminid,'Логи обновлены!')
+			restartlog()
+			time.sleep(1)
+	pass
+
+t = threading.Thread(target=logres, name='Thread1',)
+t.start()
+
+
 markup = types.ReplyKeyboardMarkup(row_width=2)
 itembtn1 = types.KeyboardButton('💾Софт💾')
 itembtn2 = types.KeyboardButton('🔒Базы🔒')
-itembtn3 = types.KeyboardButton('💩Админы💩') # Это кнопки / Buttons
+itembtn3 = types.KeyboardButton('✉️Чат✉️') # Это кнопки / Buttons
 itembtn4 = types.KeyboardButton('😁Помощь😁')
 itembtn4 = types.KeyboardButton('2>')
 markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
@@ -77,7 +96,7 @@ itembtn6 = types.KeyboardButton('<1')
 markup2.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5, itembtn6)
 
 markup3 = types.ReplyKeyboardMarkup(row_width=2)
-itembtn1 = types.KeyboardButton('Not found')  # Это кнопки / Buttons
+itembtn1 = types.KeyboardButton('💩Админы💩')
 itembtn2 = types.KeyboardButton('<2')
 markup3.add(itembtn1, itembtn2)
 
@@ -85,12 +104,12 @@ admmarkup = types.ReplyKeyboardMarkup(row_width=2)
 itembtn1 = types.KeyboardButton('/addlink')
 itembtn2 = types.KeyboardButton('/addbase')
 itembtn3 = types.KeyboardButton('/dbupload')
-itembtn3 = types.KeyboardButton('/addfriend')
-itembtn4 = types.KeyboardButton('Exit')
-admmarkup.add(itembtn1, itembtn2, itembtn3, itembtn4)
-###################
+itembtn4 = types.KeyboardButton('/addfriend')
+itembtn5 = types.KeyboardButton('Exit')
+admmarkup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5)
+
 @bot.message_handler(content_types=['text', '/start'])
-###################
+
 def send_welcome(message):
 	global logs, myString, idslist
 	if message.text == '/start':
@@ -113,12 +132,12 @@ def send_welcome(message):
 		bot.send_message(message.chat.id,"Наш чат: t.me/wproject_chat")
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 	elif message.text == '💾Софт💾' or message.text == '/soft':
-		links2 = open('links.txt','r')
+		links2 = open('Other/links.txt','r')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.send_message(message.chat.id, '💾Список программ и ссылок на их: \n' + links2.read() + '\n💾')
 		links2.close()
 	elif message.text == '🔒Базы🔒'  or message.text == '/dblist':
-		base2 = open('base.txt','r')
+		base2 = open('Other/base.txt','r')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.send_message(message.chat.id, '📁Список баз данных и ссылок на их: \n' + base2.read() + '\n📁')
 		base2.close()
@@ -138,58 +157,61 @@ def send_welcome(message):
 		bot.send_message(message.chat.id, 'Страница №3', reply_markup=markup3)
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 	elif message.text == '/friends' or message.text == '👨‍👩‍👧‍👦Друзья👨‍👩‍👧‍👦':
-		friendbase = open('friends.txt','r')
+		friendbase = open('Other/friends.txt','r')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.send_message(message.chat.id, 'Наши друзья: \n' + friendbase.read())
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 	elif message.text == '/stat' or message.text == '📊Статистика📊':
-		idslist.close()  #проблема в статистке с которой я еще списал пару команд и получил ахуенные баги
-		idslist = open('idlist.txt','r')
-		with open('idlist.txt') as idslist:
+		idslist.close()
+		idslist = open('Other/idlist.txt','r')
+		with open('Other/idlist.txt') as idslist:
 			size=sum(1 for _ in idslist)
 			bot.send_message(message.chat.id, '📊Статистика отображается в реальном времени!📊\nПользователей🙎‍♂: '+ str(size))
 			idslist.close()
-			idslist = open('idlist.txt','a')
+			idslist = open('Other/idlist.txt','a')
 			pass
 	elif message.text == '🔎GitHub🔍' or  message.text == '/github':
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n')
-		bot.send_message(message.chat.id, '🔎Страница бота на GitHub - https://github.com/d2nekomet/WBot-TelegramBot  🔍')
+		bot.send_message(message.chat.id, '🔎Страница бота на GitHub - https://github.com/d2nekomet/WBot-TelegramBot \n🔍')
 	elif message.text == '/payments' or message.text == '💳Реквизиты💳':
 		bot.send_message(message.chat.id, '''Привет. Всем надо зарабатывать и кушать. Админ этого бота и канала @www_project не исключение. Однако я не создаю приватные группы и т.д. Почему? Я считаю, что информация должна быть бесплатной!
 Поэтому прошу помочь проекту рублем и украсить вечер админа порцией кексиков, ну или стену канала еще одним годным постом сделанным благодаря Вашим поддержкам!
 Спасибо :)
 Реквизиты:
 Qiwi - https://qiwi.me/wwwproject
-QiWi Карта - 
+QiWi Карта - 4890494672490182
 BTC - 1EwW7KwrEr5w2UXeCnJdJKAGPuWQPS1ZfV
 Ethereum: 0xdb05ab0547e28f62ad0c7d856c0b9b4ed6d28789
 Спасибо!❤️
 ''')
-#       СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ  
-#       СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ  
-#       СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ   СЛУЖЕБНЫЕ КОМАНДЫ  
 	elif message.text == '/addlink' and message.chat.id == adminid:
 		l = bot.send_message(message.chat.id, 'Введите <имя файла> <ссылку на файл>:')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.register_next_step_handler(l, url_links)
+
 	elif message.text == '/addbase' and message.chat.id == adminid:
 		b = bot.send_message(message.chat.id, 'Введите <имя базы> <ссылку на базу>:')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.register_next_step_handler(b, url_base)
+
 	elif message.text == '/addfriend' and message.chat.id == adminid:
 		fr = bot.send_message(message.chat.id, 'Введите то, что надо ввести чтобы добавить друзей в список:')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.register_next_step_handler(fr, friend_base)
+
 	elif message.text == '/dbupload' and message.chat.id == adminid:
 		myDBList = os.listdir(path="Logs/")
 		myDBString = '	,	'.join(myDBList)
 		db = bot.send_message(message.chat.id, 'Выберите базу которую хотите скачать (напишите имя файла): \n' + myDBString)
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 		bot.register_next_step_handler(db, dbupl)
+
 	elif message.text == '/adminka' and message.chat.id == adminid:
 		bot.send_message(adminid,"Админ панель открыта!", reply_markup=admmarkup)
+
 	elif message.text == 'Exit' and message.chat.id == adminid:
 		bot.send_message(adminid, 'Вы вышли из админ панели!', reply_markup=markup)
+
 	msg = message.text.split()
 	if msg[0] == '/send':
 		myList = msg[2:]
@@ -198,6 +220,8 @@ Ethereum: 0xdb05ab0547e28f62ad0c7d856c0b9b4ed6d28789
 		bot.send_message(adminid, '✉️Соощение пользователю ' + msg[1] + ' отправлено успешно!\n✉️Текст сообщения: ' + str(myString) + ' ✉️')
 		logs.write('Сообщения: ' + message.text + '\nВремя получения: ' + time.ctime() + '\nАйди: '+ str(message.chat.id) +'\nИмя: ' + str(message.from_user.first_name) + '\nФамилия: ' + str(message.from_user.last_name) + '\nНик: @' + str(message.from_user.username)+ '\n' + 'Тип чата: '+ str(message.chat.type) +'\n\n') 
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG) #логи в консоль
+logging.basicConfig(filename="sample.log", level=logging.DEBUG)
+logging.debug("\nDebug " + time.ctime() + '\n')
+logging.info("\nInformational" + time.ctime()+ '\n')
+logging.error("\n!!!ERROR!!!" + time.ctime()+ '\n')
 bot.polling(none_stop=True)
